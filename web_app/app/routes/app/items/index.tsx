693ccf95345e7de8876b121baa42daf9058079b6
KPI01 <jlrd.dev@outlook.com>
2025-02-27 21:57:41 +0100

    feat: creado componente de query en tabla

--- web_app/app/routes/app/items/index.tsx
+++ web_app/app/routes/app/items/index.tsx
@@ -1,22 +1,18 @@
-import DataTable from "~/components/table/data-table";
-import { itemColumn } from "./tables";
 import { PrismaClient } from "@prisma/client";
-import { type MetaFunction, data } from "react-router";
-import { Flex, Grid, Heading } from "@radix-ui/themes";
-import {
-  type ColumnFiltersState,
-  type ColumnDef,
-  type ColumnFilter,
-} from "@tanstack/react-table";
-import { InputField } from "~/components/forms/input";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
-import type { Route } from "./+types";
-import SelectInput from "~/components/forms/select";
+import { Button, Flex, Grid, Heading } from "@radix-ui/themes";
+import { type ColumnDef, type ColumnFiltersState } from "@tanstack/react-table";
+import { Label } from "radix-ui";
 import { useState, type ChangeEvent } from "react";
+import { type MetaFunction } from "react-router";
+import { InputField } from "~/components/forms/input";
+import SelectInput from "~/components/forms/select";
+import DataTable from "~/components/table/data-table";
+import type { Route } from "./+types";
+import { itemColumn } from "./tables";
+import { Item } from "@radix-ui/themes/components/segmented-control";
+import TableQuery from "./forms/tableQuery";
+
+const prisma = new PrismaClient();
 
 export const meta: MetaFunction = () => {
   return [
@@ -24,33 +20,53 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export async function loader() {
-  const prisma = new PrismaClient();
-  const items = await prisma.item.findMany({
-    include: { unidadMedida: true, ubicacion: true, stock: true },
+export async function loader({ request }: Route.LoaderArgs) {
+  let items = [];
+  const url = new URL(request.url);
+  const relations = url.searchParams.getAll("relations");
+
+  if (relations.length > 0) {
+    console.debug("relations:", relations);
+
+    items = await prisma.item.findMany({
+      include: {
+        unidadMedida: relations.includes("unidadMedida"),
+        ubicacion: relations.includes("ubicacion"),
+        stock: relations.includes("stock"),
+      },
+    });
+
+    return { items };
+  }
+
+  items = await prisma.item.findMany({
+    include: {
+      unidadMedida: true,
+      ubicacion: true,
+      stock: true,
+    },
   });
 
   return { items };
 }
 
+export async function action({}: Route.ActionArgs) {
+  const items = await prisma.item.findMany({ include: { unidadMedida: true } });
+
+  return { items };
+}
+
 export default function Index({ loaderData }: Route.ComponentProps) {
+  const { items } = loaderData;
+
   const columns = {
     id: "Codigo",
     descripcion: "Descripcion",
   };
-  const [column, setColumn] = useState<keyof typeof columns>("id");
+
   const [filter, setFilter] = useState<ColumnFiltersState>([]);
   const [showData, setShowData] = useState(false);
   console.debug("table filters:", filter);
-
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    const input = e.target.value;
-    console.debug(`valor para [${column}]:`, input);
-
-    if (input !== "") setShowData(true);
-
-    setFilter([{ id: column, value: input }]);
-  }
 
   return (
     <Grid gapY="6">
@@ -58,23 +74,25 @@ export default function Index({ loaderData }: Route.ComponentProps) {
         Consulta de Articulo
       </Heading>
       <Flex align="center" justify="start" gapX="2">
-        <SelectInput
-          name="column"
+        <TableQuery
           options={columns}
-          state={{
-            value: column,
-            changer: setColumn,
+          changeColumnCallback={() => {
+            setFilter([]);
+            setShowData(false);
           }}
-        />
-        <InputField
-          input={{
-            type: "text",
-            onChange: handleChange,
+          changeQueryCallback={(col, val) => {
+            setFilter([{ id: col, value: val }]);
+            if (val !== "") setShowData(true);
           }}
+          clearQueryCallback={() => {
+            setFilter([]);
+            setShowData(false);
+          }}
+          clearAction
         />
       </Flex>
       <DataTable
-        data={showData ? loaderData.items : []}
+        data={showData ? items : []}
         columns={itemColumn as ColumnDef<any>[]}
         state={{
           filter,
--- web_app/app/routes/app/items/stock.tsx
+++ web_app/app/routes/app/items/stock.tsx
@@ -6,10 +6,11 @@ import { stockColumn } from "./tables";
 import { Button, Flex, Grid, Heading } from "@radix-ui/themes";
 import { type ColumnDef, type ColumnFiltersState } from "@tanstack/react-table";
 import type { Route } from "./+types/stock";
-import { useMemo, useState, type ChangeEvent } from "react";
+import { useState, type ChangeEvent } from "react";
 import SelectInput from "~/components/forms/select";
 import { InputField } from "~/components/forms/input";
 import { Label } from "radix-ui";
+import TableQuery from "./forms/tableQuery";
 
 const prisma = new PrismaClient();
 
@@ -40,57 +41,23 @@ export default function Stock({ loaderData }: Route.ComponentProps) {
     item: "Artículo",
     ubicacion: "Ubicación",
   };
-  const [column, setColumn] = useState<keyof typeof columns>("item");
-  const [query, setQuery] = useState<string>("");
+
   const [filter, setFilter] = useState<ColumnFiltersState>([]);
-
-  function handleColumnChange(value: keyof typeof columns) {
-    setQuery("");
-    setFilter([]);
-    setColumn(value);
-  }
-
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    const input = e.target.value;
-    console.debug(`valor para [${column}]:`, input);
-
-    setQuery(input);
-    setFilter([{ id: column, value: input }]);
-  }
-
-  function clearInput() {
-    setQuery("");
-    setFilter([]);
-  }
 
   return (
     <Grid gapY="8">
       <Heading as="h1" size="9">
         Rotación de Material
       </Heading>
-      <Grid columns="1" gapY="2">
-        <Label.Root>Ingresa un valor para iniciar la búsqueda:</Label.Root>
-        <Flex gapX="3" align="center">
-          <SelectInput
-            name="column"
-            options={columns}
-            state={{
-              value: column,
-              changer: handleColumnChange,
-            }}
-          />
-          <InputField
-            input={{
-              type: column === "fecha" ? "date" : "text",
-              value: query,
-              onChange: handleChange,
-            }}
-          />
-          <Button color="red" variant="outline" onClick={() => clearInput()}>
-            Limpiar
-          </Button>
-        </Flex>
-      </Grid>
+      <TableQuery
+        options={columns}
+        changeColumnCallback={() => {
+          setFilter([]);
+        }}
+        changeQueryCallback={(col, val) => {
+          setFilter([{ id: col, value: val }]);
+        }}
+      />
       <DataTable
         data={stock}
         columns={stockColumn as ColumnDef<any>[]}
