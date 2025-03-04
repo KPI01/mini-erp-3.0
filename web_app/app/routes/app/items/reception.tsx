077d599150327475726f0a48763205c1a2c8741b
KPI01 <jlrd.dev@outlook.com>
2025-03-04 21:50:31 +0100

    chore: PageHeader
    creado componente para los titulos de las páginas

--- web_app/app/routes/app/items/reception.tsx
+++ web_app/app/routes/app/items/reception.tsx
@@ -7,6 +7,7 @@ import type { Route } from "./+types/reception";
 import type { SelectInputOptionsType } from "~/types/components";
 import { addStock } from "~/server/actions/stock.server";
 import { AddStockForm } from "./forms/stock";
+import { PageHeader } from "~/components/ui/header";
 
 const prisma = new PrismaClient();
 
@@ -60,9 +61,7 @@ export default function Reception({ loaderData }: Route.ComponentProps) {
   });
   return (
     <Grid height="100%" style={{ gridAutoRows: "auto 1fr" }}>
-      <Heading as="h1" size="9">
-        Recepción de Material
-      </Heading>
+      <PageHeader title="Recepción de Material" />
       <Box width="40vw" m="auto">
         <CardContructor contentProps={{ px: "6", py: "4" }}>
           <AddStockForm aux={{ ubicaciones }} />
